45044a8171dfb84ceb715ff618762d973d36c79c
Maartje Eyskens <maartje@eyskens.me>
2019-05-22 15:38:18 +0200

    Add ghost-gcs-backup
    ghosth-gcs-backup is a Docker image with a Node.Js script which fetches
    posts from the Ghost API using the API key.
    Then it uploads the received content to GCS toghether with all images used.
    
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

---
+++ ghost-gcs-backup/script/backup.js
@@ -0,0 +1,60 @@
+// add timestamps in front of log messages
+require('console-stamp')(console, '[HH:MM:ss]');
+
+if (!process.env.GHOST_ENDPOINT) {
+    console.error("Misssing GHOST_ENDPOINT environment variable");
+    process.exit(1);
+}
+
+if (!process.env.GHOST_API_KEY) {
+    console.error("Misssing GHOST_API_KEY environment variable");
+    process.exit(1);
+}
+
+if (!process.env.GCS_BUCKET) {
+    console.error("Misssing GCS_BUCKET environment variable");
+    process.exit(1);
+}
+
+const fs = require('fs');
+const https = require('https');
+const GhostContentAPI = require('@tryghost/content-api');
+const { Storage } = require('@google-cloud/storage');
+const storage = new Storage();
+
+const today = new Date();
+const dd = String(today.getDate()).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, '0');
+const yyyy = today.getFullYear();
+
+const downloadImage = (src) => new Promise(async (resolve, reject) => {
+    const file = await storage.bucket(process.env.GCS_BUCKET).file(`ghost_${yyyy}_${mm}_${dd}` + src).createWriteStream()
+    https.get(process.env.GHOST_ENDPOINT + src, function(response) {
+        response.pipe(file)
+        .on('error', (err) => reject(err))
+        .on('finish', () => resolve());
+    });
+})
+
+const api = new GhostContentAPI({
+    url: process.env.GHOST_ENDPOINT,
+    version: 'v2',
+    key: process.env.GHOST_API_KEY,
+});
+
+const run = async () => {
+    console.log("Fetching posts")
+    const posts = await api.posts.browse({'limit': 1000});
+    await storage.bucket(process.env.GCS_BUCKET).file(`ghost_${yyyy}_${mm}_${dd}.json`).save(JSON.stringify(posts))
+
+    console.log("Fetching images")
+    for (let post of posts) {
+        await downloadImage(post.feature_image.substr(process.env.GHOST_ENDPOINT.length));
+        for (let card of JSON.parse(post.mobiledoc).cards) {
+            if (card[0] == 'image') {
+                await downloadImage(card[1].src);
+            }
+        }
+    }
+}
+run();
